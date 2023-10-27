22f23c491fc6526440b376edb6ae50b29039e36c
Bergomezz <bernardogomezz@yahoo.com.br>
2023-10-27 17:02:39 -0300

    adding information

--- run-test.ps1
+++ run-test.ps1
@@ -1,4 +1,4 @@
-$BROWSER=@('chrome', 'firefox', 'safari', 'edge')
+$BROWSER=@('locally-installed:chrome', 'firefox', 'safari', 'edge')
 
 foreach ($browser in $BROWSER){
   cross-env BROWSER_LIST=$browser testcafe $env:BROWSER_LIST ./tests -s takeOnFails=true
