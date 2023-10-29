bbbe6e6f8ec2bf05ab8da63d40241c81cf1d50d5
Bernardo Gomes Parente <88146625+Bergomezz@users.noreply.github.com>
2023-10-29 12:20:24 -0300

    Web automation testing (#5)
    * feature: login test
    
    * feature: yaml file and shell file
    
    * adding information
    
    * feature: script to test in all browsers informed
    
    * feature: login test with invalid user and password

--- tests/login.test.js
+++ tests/login.test.js
@@ -7,8 +7,17 @@ fixture `Login Test`
 test('User cannot login with invalid credentials', async t => {
   const signInButton = Selector('#signin_button');
   const loginForm = Selector('#login_form');
+  const usernameInput = Selector('#user_login');
+  const passwordInput = Selector('#user_password');
+  const submitButton = Selector('.btn-primary');
+  const errorMessage = Selector('.alert-error').innerText;
 
   await t.click(signInButton);
 
   await t.expect(loginForm.exists).ok();
+  await t.typeText(usernameInput, 'invalid username', { paste: true });
+  await t.typeText(passwordInput, 'wrong password', { paste: true });
+  await t.click(submitButton);
+
+  await t.expect(errorMessage).contains('Login and/or password are wrong');
 });
