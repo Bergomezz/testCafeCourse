b7a946d0b4e8d2b71ca8c4d8bf2e060a2918085c
Bernardo Gomes Parente <88146625+Bergomezz@users.noreply.github.com>
2023-10-30 14:10:56 -0300

    feature: valid login test script (#6)

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,7 +2,10 @@ import { Selector } from 'testcafe';
 
 // prettier-ignore
 fixture `Login Test`
-  .page `http://zero.webappsecurity.com/index.html`;
+  .page `http://zero.webappsecurity.com/index.html`
+.beforeEach(async t => {
+  await t.navigateTo('http://zero.webappsecurity.com/index.html');
+});
 
 test('User cannot login with invalid credentials', async t => {
   const signInButton = Selector('#signin_button');
@@ -16,8 +19,46 @@ test('User cannot login with invalid credentials', async t => {
 
   await t.expect(loginForm.exists).ok();
   await t.typeText(usernameInput, 'invalid username', { paste: true });
-  await t.typeText(passwordInput, 'wrong password', { paste: true });
-  await t.click(submitButton);
+  await t
+    .typeText(passwordInput, 'wrong password', { paste: true })
+    .pressKey('enter');
+  //await t.click(submitButton);
 
   await t.expect(errorMessage).contains('Login and/or password are wrong');
+});
+
+test('User can login into application using a valid credentials', async t => {
+  // variables
+  const signInButton = Selector('#signin_button');
+  const loginForm = Selector('#login_form');
+  const usernameInput = Selector('#user_login');
+  const passwordInput = Selector('#user_password');
+  const submitButton = Selector('.btn-primary');
+  const accountSummaryTab = Selector('#account_summary_tab');
+  const userIcon = Selector('.icon-user');
+  const logoutButton = Selector('#logout_link');
+
+  // Tests
+  await t.click(signInButton);
+
+  await t.expect(loginForm.exists).ok();
+  await t.typeText(usernameInput, 'username', { paste: true });
+  await t
+    .typeText(passwordInput, 'password', { paste: true })
+    .pressKey('enter');
+  //await t.click(submitButton);
+
+  // Assertions
+  await t.expect(accountSummaryTab.exists).ok();
+  await t.expect(loginForm.exists).notOk();
+  await t.expect(userIcon.visible).ok();
+
+  //finishing the test
+  await t.click(logoutButton);
+  await t.expect(logoutButton.visible).notOk();
+  await t.expects(signInButton.visible).ok();
+}).timeouts({
+  requestTimeout: 5000,
+  pageRequestTimeout: 60000,
+  ajaxRequestTimeout: 60000,
 });
