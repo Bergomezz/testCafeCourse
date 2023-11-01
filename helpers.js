38308d788b7dbfdec77bf3cd4b9f2018528575c7
Bergomezz <bernardogomezz@yahoo.com.br>
2023-11-01 14:20:22 -0300

    feature: helpers functions and payee test

---
+++ helpers.js
@@ -0,0 +1,12 @@
+import { t } from 'testcafe';
+import { Locators } from './Class/Locators';
+
+const localize = new Locators();
+
+export async function Login(username, password) {
+  await t.click(localize.signInButton);
+  await t.typeText(localize.usernameInput, username, { paste: true });
+  await t
+    .typeText(localize.passwordInput, password, { paste: true })
+    .click(localize.submitButton);
+}
---
+++ tests/homepage/searchingInput.test.js
@@ -0,0 +1,31 @@
+import { Selector, RequestLogger } from 'testcafe';
+import { Locators } from '../../Class/Locators';
+
+const localize = new Locators();
+
+const logger = RequestLogger();
+
+// prettier-ignore
+fixture `Searching Input Box`
+  .page `http://zero.webappsecurity.com/index.html`
+  .requestHooks(logger)
+
+test('searching box funtionality. User can searching for information using it', async t => {
+  // Variable
+  const searchBoxSelector = Selector('h2').innerText;
+  const anchorLink = Selector('a').withAttribute('href');
+
+  // 1st action
+  await t
+    .expect(logger.contains(record => record.response.statusCode === 200))
+    .ok();
+
+  // 2nd action
+  await t
+    .typeText(localize.searchingBoxInput, 'online banking', { paste: true })
+    .pressKey('enter');
+
+  // Assert
+  await t.expect(searchBoxSelector).contains('Search Results:');
+  await t.expect(anchorLink.exists).ok();
+});
