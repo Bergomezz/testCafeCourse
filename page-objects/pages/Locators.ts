36b172bc05b287a15de9e097aa39600dccd81ae5
Bernardo Gomes Parente <88146625+Bergomezz@users.noreply.github.com>
2023-11-06 16:34:46 -0300

    Update Locators.ts

--- page-objects/pages/Locators.ts
+++ page-objects/pages/Locators.ts
@@ -1,20 +1,20 @@
 import { Selector, t } from 'testcafe';
 
 export default class Locators {
-  loginForm = Selector('#login_form');
-  usernameInput = Selector('#user_login');
-  passwordInput = Selector('#user_password');
-  submitButton = Selector('.btn-primary');
-  feedbackButton = Selector('#feedback');
-  signInButton = Selector('#signin_button');
-  feebackForm = Selector('.form-inputs');
-  nameInputField = Selector('#name');
-  emailInputField = Selector('#email');
-  subjectInputField = Selector('#subject');
-  messageTextArea = Selector('#comment');
-  senderButton = Selector('.btn-signin');
-  onlineBakingButton = Selector('#onlineBankingMenu');
-  searchingBoxInput = Selector('#searchTerm');
-  payBillsTab = Selector('#pay_bills_tab');
-  sendPage = Selector('div');
+  loginForm: Selector = Selector('#login_form');
+  usernameInput: Selector = Selector('#user_login');
+  passwordInput: Selector = Selector('#user_password');
+  submitButton: Selector = Selector('.btn-primary');
+  feedbackButton: Selector = Selector('#feedback');
+  signInButton: Selector = Selector('#signin_button');
+  feebackForm: Selector = Selector('.form-inputs');
+  nameInputField: Selector = Selector('#name');
+  emailInputField: Selector = Selector('#email');
+  subjectInputField: Selector = Selector('#subject');
+  messageTextArea: Selector = Selector('#comment');
+  senderButton: Selector = Selector('.btn-signin');
+  onlineBakingButton: Selector = Selector('#onlineBankingMenu');
+  searchingBoxInput: Selector = Selector('#searchTerm');
+  payBillsTab: Selector = Selector('#pay_bills_tab');
+  sendPage: Selector = Selector('div');
 }
